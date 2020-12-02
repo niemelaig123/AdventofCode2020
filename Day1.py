48954b59df21b22d605e079f79fb85c0c16fb184
Gideon <58040438+niemelaig123@users.noreply.github.com>
2020-12-01 20:09:32 -0400

    Day 1

---
+++ Day1.py
@@ -0,0 +1,31 @@
+"""
+Student Name:    
+Program Title:  
+Description:    
+"""
+
+def main(): #<-- Don't change this line!
+    #Write your code below. It must be indented!
+    fileSelected = "Files\day1.txt"
+    openMode = "r"
+    myFile = open(fileSelected, openMode)
+    fileContents = myFile.read().split("\n")
+    for i in range(len(fileContents)):
+        for j in range(len(fileContents)):
+            for k in range(len(fileContents)):
+                if int(fileContents[i]) + int(fileContents[j]) + int(fileContents[k]) == int(2020):
+                    multiple1 = int(fileContents[i])
+                    multiple2 = int(fileContents[j])
+                    multiple3 = int(fileContents[k])
+                    product = multiple1 * multiple2 * multiple3
+                    print(int(multiple1))
+                    print(int(multiple2))
+                    print(int(multiple3))
+                    print(int(product))
+
+
+    #Your code ends on the line above
+
+#Do not change any of the code below!
+if __name__ == "__main__":
+    main()
